fe0d726ca53145449c4dde382d982db7da6121a1
Wai Linn Aung <waia00198@gmail.com>
2023-11-12 15:52:42 +0630

    fix .env error

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config({ path: "../SocialApp/.env.local" });
 
 const typeDefs = `
     type Query{
@@ -20,11 +21,12 @@ const server = new ApolloServer({
   resolvers,
 });
 
-mongoose.connect('mongodb+srv://root:YcEnkIj5BQHS6JC2@bookstore-mern.6fqy9sb.mongodb.net/?retryWrites=true&w=majority');
-mongoose.connection.once("open",() => console.log("Database Connected"));
+mongoose.connect(process.env.MONGODB);
+mongoose.connection.once("open", () => console.log("Database Connected"));
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
+
